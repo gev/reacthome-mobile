196af1268e506636b6d8ef43fbb703e2959a57ad
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-10-12 01:46:30 +0300

    add a type

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,15 @@
+import 'package:flutter/widgets.dart';
 import 'package:reacthome/app/app.android.dart';
 import 'package:reacthome/app/app.ios.dart';
 import 'package:reacthome/app/navigation.dart';
 import 'package:reacthome/domains/discovery/discovery.dart';
 import 'package:reacthome/platform/platform.dart';
 
-makeApp(
-        {required Discovery discovery,
-        required Navigation navigation,
-        String title = 'Reacthome'}) =>
+Widget makeApp({
+  required Discovery discovery,
+  required Navigation navigation,
+  String title = 'Reacthome',
+}) =>
     selectPlatform(
       ios: AppIOS(discovery, navigation, title),
       android: AppAndroid(discovery, navigation, title),
