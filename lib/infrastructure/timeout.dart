f6b9b9b04718f93d6041fdcbfc57f4a2f731591e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-10-21 15:13:40 +0300

    timeouts

---
+++ lib/infrastructure/timeout.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+class Timeout<K> {
+  final Map<K, Timer> _timers = {};
+
+  void start({
+    required K id,
+    required Duration duration,
+    required void Function() execute,
+  }) {
+    _timers[id]?.cancel();
+    _timers[id] = Timer(duration, execute);
+  }
+
+  void cancel(K id) {
+    _timers[id]?.cancel();
+    _timers.remove(id);
+  }
+
+  void dispose() {
+    for (final timer in _timers.values) {
+      timer.cancel();
+    }
+  }
+}
