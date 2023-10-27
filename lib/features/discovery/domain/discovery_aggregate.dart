58eab9e161b6d5ac1b9d3a6153eca42e69ff5e80
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-10-27 21:02:08 +0300

    use only Map

--- lib/features/discovery/domain/discovery_aggregate.dart
+++ lib/features/discovery/domain/discovery_aggregate.dart
@@ -3,17 +3,15 @@ import 'package:reacthome/features/discovery/domain/discovery_daemon.dart';
 
 class DiscoveryAggregate {
   final Map<String, DiscoveryDaemon> _map;
-  final List<String> _list;
 
-  DiscoveryAggregate(this._map, this._list);
+  DiscoveryAggregate(this._map);
 
-  Iterable<String> get daemons => _list;
+  Iterable<String> get daemons => _map.keys;
 
   DiscoveryDaemon? getDaemonById(String id) => _map[id];
 
   DiscoveryEvent addDaemon(String id, DiscoveryDaemon daemon) {
     if (!_map.keys.contains(id)) {
-      _list.add(id);
       _map[id] = daemon;
       return DiscoveryEventDaemonAdded(id);
     } else if (_map[id] != daemon) {
@@ -25,7 +23,6 @@ class DiscoveryAggregate {
 
   DiscoveryEvent? removeDaemon(String id) {
     if (_map.containsKey(id)) {
-      _list.remove(id);
       _map.remove(id);
       return DiscoveryEventDaemonRemoved(id);
     }
--- lib/features/discovery/infrastructure/discovery_repository.dart
+++ lib/features/discovery/infrastructure/discovery_repository.dart
@@ -2,7 +2,7 @@ import 'package:reacthome/features/discovery/application/discovery_service.dart'
 import 'package:reacthome/features/discovery/domain/discovery_aggregate.dart';
 
 class DiscoveryRepository implements Discovery {
-  final DiscoveryAggregate _daemons = DiscoveryAggregate({}, []);
+  final DiscoveryAggregate _daemons = DiscoveryAggregate({});
 
   @override
   DiscoveryAggregate get daemons => _daemons;
