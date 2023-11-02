fc3277c7e226188bb5ee0a7fba9662772387bcf3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-11-02 18:00:14 +0300

    typo

--- lib/ui/screens/home/home_screen_daemon_tile_view.dart
+++ lib/ui/screens/home/home_screen_daemon_tile_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:reacthome/ui/screens/home/home_screen_daemon_tile_view.android.dart';
-import 'package:reacthome/ui/screens/home/home_screen_daemon_tile_view.ios%20copy.dart';
+import 'package:reacthome/ui/screens/home/home_screen_daemon_tile_view.ios.dart';
 import 'package:reacthome/util/platform.dart';
 
 Widget daemonTile({
