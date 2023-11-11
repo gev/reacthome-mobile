085d6a88cdba9d0ee92b0f23ae9cfc756add2dc1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-11-11 22:46:38 +0300

    use immutable map

--- lib/util/repository.dart
+++ lib/util/repository.dart
@@ -1,15 +1,16 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:reacthome/util/entity.dart';
 
 abstract interface class Repository<K, V extends Entity<K>> {
   Iterable<K> getAll();
   bool has(K id);
-  V? get(String id);
+  V? get(K id);
   void add(V daemon);
   void remove(K id);
 }
 
 class InMemoryRepository<K, V extends Entity<K>> implements Repository<K, V> {
-  final Map<K, V> _store = {};
+  IMap<K, V> _store = IMap();
 
   @override
   Iterable<K> getAll() => _store.keys;
@@ -18,11 +19,11 @@ class InMemoryRepository<K, V extends Entity<K>> implements Repository<K, V> {
   bool has(K id) => _store.containsKey(id);
 
   @override
-  V? get(String id) => _store[id];
+  V? get(K id) => _store.get(id);
 
   @override
-  void add(V entity) => _store[entity.id] = entity;
+  void add(V entity) => _store = _store.add(entity.id, entity);
 
   @override
-  void remove(K id) => _store.remove(id);
+  void remove(K id) => _store = _store.remove(id);
 }
