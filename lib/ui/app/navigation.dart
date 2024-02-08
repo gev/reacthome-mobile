ea1c6e36dfe9b64b2b6695a3f97f85bb2baad147
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-08 16:49:44 +0300

    Add screens to the navigation

--- lib/ui/app/navigation.dart
+++ lib/ui/app/navigation.dart
@@ -2,19 +2,30 @@ import 'package:flutter/widgets.dart';
 
 class NavigationRouteNames {
   static const splash = 'splash';
+  static const welcome = 'welcome';
+  static const discovery = 'discovery';
   static const home = '/';
 }
 
 class Navigation {
-  final WidgetBuilder home;
   final WidgetBuilder splash;
+  final WidgetBuilder welcome;
+  final WidgetBuilder discovery;
+  final WidgetBuilder home;
 
-  const Navigation({required this.home, required this.splash});
+  const Navigation({
+    required this.splash,
+    required this.welcome,
+    required this.discovery,
+    required this.home,
+  });
 
   String get initialRoute => NavigationRouteNames.splash;
 
   Map<String, WidgetBuilder> get routes => ({
-        NavigationRouteNames.splash: splash,
         NavigationRouteNames.home: home,
+        NavigationRouteNames.splash: splash,
+        NavigationRouteNames.welcome: welcome,
+        NavigationRouteNames.discovery: discovery,
       });
 }
