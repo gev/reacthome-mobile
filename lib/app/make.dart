a9dba7165dc1469d1a453020b7a7fbacf6845e22
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-08 16:49:07 +0300

    Add the welcome and discovery screens

--- lib/app/make.dart
+++ lib/app/make.dart
@@ -4,8 +4,10 @@ import 'package:reacthome/app/features/app_life_cycle_factory.dart';
 import 'package:reacthome/app/features/connection_factory.dart';
 import 'package:reacthome/app/features/daemon_connection_factory.dart';
 import 'package:reacthome/app/features/discovery_factory.dart';
+import 'package:reacthome/app/ui/screens/discovery_screen_factory.dart';
 import 'package:reacthome/app/ui/screens/home_screen_factory.dart';
 import 'package:reacthome/app/ui/screens/splash_screen_factory.dart';
+import 'package:reacthome/app/ui/screens/welcome_screen_factory.dart';
 import 'package:reacthome/ui/app/app.dart';
 import 'package:reacthome/ui/app/navigation.dart';
 
@@ -26,6 +28,8 @@ Widget make() {
       appLifeCycle: AppLifecycleFactory.instance.makeAppLifecycleService(),
       navigation: Navigation(
         splash: SplashScreenFactory.instance.make,
+        welcome: WelcomeScreenFactory.instance.make,
+        discovery: DiscoveryScreenFactory.instance.make,
         home: HomeScreenFactory.instance.make,
       ));
 }
