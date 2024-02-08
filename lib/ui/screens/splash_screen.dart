57688831653386d31b03605a928b7942fe3e41aa
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-08 16:47:10 +0300

    i18n + logo

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,20 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:reacthome/ui/app/navigation.dart';
-import 'package:reacthome/ui/assets.dart';
+import 'package:reacthome/ui/widgets/logo.dart';
 import 'package:reacthome/ui/widgets/screen/screen.dart';
 
 class SplashScreen {
   static Widget make(BuildContext context) {
     Timer(const Duration(seconds: 1), () {
-      Navigator.of(context).pushReplacementNamed(NavigationRouteNames.home);
+      Navigator.of(context).pushReplacementNamed(NavigationRouteNames.welcome);
     });
     return Screen.make(
-      body: Center(
-        child: SvgPicture.asset(Assets.logo, width: 100),
-      ),
+      body: const Center(child: Logo()),
     );
   }
 }
