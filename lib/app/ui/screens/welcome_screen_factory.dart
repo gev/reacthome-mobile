1b8a0060e9f90bb1a52da3d984e26d8c0157c7ce
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-08 16:47:42 +0300

    The welcome screen

---
+++ lib/app/ui/screens/welcome_screen_factory.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:reacthome/ui/screens/welcome_screen.dart';
+
+class WelcomeScreenFactory {
+  static final instance = WelcomeScreenFactory._();
+
+  WelcomeScreenFactory._();
+
+  Widget make(BuildContext context) => WelcomeScreen.make(context);
+}
---
+++ lib/ui/screens/welcome_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:reacthome/ui/app/navigation.dart';
+import 'package:reacthome/ui/widgets/filled_button/filled_button.dart';
+import 'package:reacthome/ui/widgets/logo.dart';
+import 'package:reacthome/ui/widgets/screen/screen.dart';
+
+class WelcomeScreen {
+  static Widget make(BuildContext context) {
+    var locale = AppLocalizations.of(context)!;
+    return Screen.make(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Logo(),
+            const SizedBox(height: 24),
+            Text(locale.welcome, style: const TextStyle(fontSize: 28)),
+            const SizedBox(height: 24),
+            FilledButton.make(
+              label: locale.addYourHome,
+              onPressed: () => Navigator.of(context)
+                  .pushReplacementNamed(NavigationRouteNames.discovery),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
