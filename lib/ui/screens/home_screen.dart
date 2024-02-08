6a23060758f314f64416536a07fb529211f6ae35
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-08 16:46:44 +0300

    use i18n

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/widgets.dart';
+import 'package:reacthome/ui/fragments/discovery/widgets/discovery_status.dart';
 import 'package:reacthome/ui/widgets/screen/screen.dart';
 
 class HomeScreen {
   static Widget make({
-    String title = 'Home',
     required Widget left,
     required Widget right,
   }) =>
       TitleScreen.make(
-          title: title,
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [left, right],
-          ));
+        title: 'Home',
+        trailing: const DiscoveryStatus(),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [left, right],
+        ),
+      );
 }
