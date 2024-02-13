f974a0fed34422151657559c764c8767ddfb3bff
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-13 15:34:28 +0300

    Use  ListSection widget

--- lib/ui/fragments/daemon_panel/widgets/daemon_list.dart
+++ lib/ui/fragments/daemon_panel/widgets/daemon_list.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:reacthome/ui/fragments/daemon_panel/view_models/daemon_list_view_model.dart';
 import 'package:reacthome/ui/fragments/daemon_panel/widgets/daemon_tile.dart';
+import 'package:reacthome/ui/widgets/list_section/list_section.dart';
+import 'package:reacthome/ui/widgets/list_tile/list_tile_view.dart';
 
 class DaemonList extends StatelessWidget {
   const DaemonList({super.key});
@@ -12,10 +14,15 @@ class DaemonList extends StatelessWidget {
         context.select<DaemonListViewModel, Iterable<String>>(
             (model) => model.daemons);
     return Expanded(
-      child: ListView(
-        children:
-            daemons.map((id) => DaemonTile(key: ValueKey(id), id: id)).toList(),
-      ),
+      child: ListView(children: [
+        ListSection.make(
+          title: 'Available around',
+          children: daemons
+              .map((id) => DaemonTile(key: ValueKey(id), id: id))
+              .toList(),
+        ),
+        ListTile.make(title: 'Add by ID'),
+      ]),
     );
   }
 }
