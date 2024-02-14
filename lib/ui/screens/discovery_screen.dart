b4f3100a7ab063ea278abad0d3ec2266f5335ac9
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-15 01:18:01 +0300

    Use SliverAppBar

--- lib/ui/screens/discovery_screen.dart
+++ lib/ui/screens/discovery_screen.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/widgets.dart';
 import 'package:reacthome/ui/fragments/daemon/widgets/daemon_list.dart';
-import 'package:reacthome/ui/widgets/app_bar/app_bar.dart';
+import 'package:reacthome/ui/widgets/list_section/list_section.dart';
+import 'package:reacthome/ui/widgets/list_tile/list_tile.dart';
 import 'package:reacthome/ui/widgets/scaffold/scaffold.dart';
+import 'package:reacthome/ui/widgets/sliver_app_bar%20/sliver_app_bar.dart';
 
 class DiscoveryScreen {
   static Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar.build(
-          title: 'Discovery',
-        ),
-        body: const Expanded(
-          child: Column(
-            children: [
-              DaemonList(),
-            ],
-          ),
+        body: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverAppBar(title: 'Add the Home'),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              fillOverscroll: true,
+              child: Column(
+                children: [
+                  ListSection(children: [
+                    ListTile(
+                      title: 'Discovery around',
+                    ),
+                  ]),
+                  const DaemonList(title: 'Available around'),
+                  Expanded(
+                    child: ListSection(
+                      children: [
+                        ListTile(title: 'Add by ID'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       );
 }
