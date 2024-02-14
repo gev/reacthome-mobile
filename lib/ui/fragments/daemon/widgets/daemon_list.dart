b1109bd5bb56d59c9f47b4034bcf490a0e3421b7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-15 01:16:57 +0300

    make more general

--- lib/ui/fragments/daemon/widgets/daemon_list.dart
+++ lib/ui/fragments/daemon/widgets/daemon_list.dart
@@ -3,26 +3,20 @@ import 'package:provider/provider.dart';
 import 'package:reacthome/ui/fragments/daemon/view_models/daemon_list_view_model.dart';
 import 'package:reacthome/ui/fragments/daemon/widgets/daemon_tile.dart';
 import 'package:reacthome/ui/widgets/list_section/list_section.dart';
-import 'package:reacthome/ui/widgets/list_tile/list_tile_view.dart';
 
 class DaemonList extends StatelessWidget {
-  const DaemonList({super.key});
+  final String title;
+  const DaemonList({required this.title, super.key});
 
   @override
   Widget build(BuildContext context) {
     Iterable<String> daemons =
         context.select<DaemonListViewModel, Iterable<String>>(
             (model) => model.daemons);
-    return Expanded(
-      child: ListView(children: [
-        ListSection(
-          title: 'Available around',
-          children: daemons
-              .map((id) => DaemonTile(key: ValueKey(id), id: id))
-              .toList(),
-        ),
-        ListTile(title: 'Add by ID'),
-      ]),
+    return ListSection(
+      title: title,
+      children:
+          daemons.map((id) => DaemonTile(key: ValueKey(id), id: id)).toList(),
     );
   }
 }
