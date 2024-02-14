61c6f817130f9869a2226d4af676cbe69f971836
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-15 02:46:08 +0300

    use an activity indicator and a switch

--- lib/ui/screens/discovery_screen.dart
+++ lib/ui/screens/discovery_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:reacthome/ui/fragments/daemon/widgets/daemon_list.dart';
+import 'package:reacthome/ui/widgets/activity_indicator/activity_indicator.dart';
 import 'package:reacthome/ui/widgets/list_section/list_section.dart';
 import 'package:reacthome/ui/widgets/list_tile/list_tile.dart';
 import 'package:reacthome/ui/widgets/scaffold/scaffold.dart';
 import 'package:reacthome/ui/widgets/sliver_app_bar%20/sliver_app_bar.dart';
+import 'package:reacthome/ui/widgets/switch/switch.dart';
 
 class DiscoveryScreen {
   static Widget build(BuildContext context) => Scaffold(
@@ -19,13 +21,15 @@ class DiscoveryScreen {
                   ListSection(children: [
                     ListTile(
                       title: 'Discovery around',
+                      trailing: Switch(value: true, onChanged: (_) {}),
                     ),
                   ]),
                   const DaemonList(title: 'Available around'),
                   Expanded(
                     child: ListSection(
                       children: [
-                        ListTile(title: 'Add by ID'),
+                        ListTile(
+                            title: 'Add by ID', trailing: ActivityIndicator()),
                       ],
                     ),
                   ),
