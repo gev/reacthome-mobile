d40c1bd2c47a300e4f90fe69825152b16a2ab48e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-15 15:49:36 +0300

    ListTileChevron widget

---
+++ lib/ui/widgets/list_tile_chevron/list_tile_chevron_cupertino.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class ListTileChevronCupertino {
+  static WidgetBuilder build() => (_) => const CupertinoListTileChevron();
+}
---
+++ lib/ui/widgets/list_tile_chevron/list_tile_chevron_material.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ListTileChevronMaterial {
+  static WidgetBuilder build() => (_) => const Icon(Icons.chevron_right);
+}
