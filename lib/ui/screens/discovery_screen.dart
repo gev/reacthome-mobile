d45dfa48b47b91b29e2bcb5db257dce53bbf45ae
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-02-15 15:50:58 +0300

    change discovery screen

--- lib/ui/screens/discovery_screen.dart
+++ lib/ui/screens/discovery_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/widgets.dart';
+import 'package:reacthome/ui/fragments/daemon/widgets/daemon_add.dart';
 import 'package:reacthome/ui/fragments/daemon/widgets/daemon_list.dart';
-import 'package:reacthome/ui/widgets/activity_indicator/activity_indicator.dart';
-import 'package:reacthome/ui/widgets/list_section/list_section.dart';
-import 'package:reacthome/ui/widgets/list_tile/list_tile.dart';
+import 'package:reacthome/ui/fragments/discovery/widgets/discovery_status.dart';
+import 'package:reacthome/ui/widgets/divider/divider.dart';
 import 'package:reacthome/ui/widgets/scaffold/scaffold.dart';
 import 'package:reacthome/ui/widgets/sliver_app_bar%20/sliver_app_bar.dart';
-import 'package:reacthome/ui/widgets/switch/switch.dart';
 
 class DiscoveryScreen {
   static Widget build(BuildContext context) => Scaffold(
@@ -18,21 +17,12 @@ class DiscoveryScreen {
               fillOverscroll: true,
               child: Column(
                 children: [
-                  ListSection(children: [
-                    ListTile(
-                      title: 'Discovery around',
-                      trailing: Switch(value: true, onChanged: (_) {}),
-                    ),
-                  ]),
-                  const DaemonList(title: 'Available around'),
-                  Expanded(
-                    child: ListSection(
-                      children: [
-                        ListTile(
-                            title: 'Add by ID', trailing: ActivityIndicator()),
-                      ],
-                    ),
+                  const DiscoveryStatus(),
+                  DaemonList(
+                    title: 'Available around',
+                    ifEmpty: Divider(),
                   ),
+                  const Expanded(child: DaemonAdd()),
                 ],
               ),
             ),
