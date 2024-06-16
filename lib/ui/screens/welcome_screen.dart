5c4b8beebdc48558e0b8d707c1c757db1a8e9a30
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-06-17 02:21:34 +0300

    simplify welcome screen

--- lib/ui/screens/welcome_screen.dart
+++ lib/ui/screens/welcome_screen.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:reacthome/ui/fragments/welcome/widgets/welcome_landscape.dart';
-import 'package:reacthome/ui/fragments/welcome/widgets/welcome_portrait.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:reacthome/ui/app/navigation.dart';
 import 'package:reacthome/ui/kit/kit.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -9,71 +8,29 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final media = MediaQuery.of(context);
-    final isDark = media.platformBrightness == Brightness.dark;
-    final brightness = isDark ? Brightness.light : Brightness.dark;
-    final aspectRatio = media.size.width / media.size.height;
-    const transparent = Color.fromARGB(0, 0, 0, 0);
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: transparent,
-        statusBarIconBrightness: brightness,
-        systemNavigationBarColor: transparent,
-        systemNavigationBarIconBrightness: brightness,
-        systemNavigationBarContrastEnforced: true,
-      ),
-    );
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    final locale = AppLocalizations.of(context)!;
     return scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-
-            // **First universal version
-            colors: [
-              Color.fromARGB(137, 54, 218, 247),
-              Color.fromARGB(139, 14, 26, 114),
-            ],
-            // colors: isDark
-            //     ? const [
-            //         // **Cold Dark
-            //         // Color.fromARGB(159, 46, 237, 234),
-            //         // Color.fromARGB(170, 62, 175, 223),
-            //         // Color.fromARGB(206, 120, 63, 194),
-
-            //         // **Night Dark
-            //         Color.fromARGB(209, 66, 16, 147),
-            //         Color.fromARGB(122, 35, 19, 123),
-            //         Color.fromARGB(99, 2, 0, 45),
-            //       ]
-            //     : const [
-            //         // **Cold Light
-            //         // Color.fromARGB(129, 108, 231, 217),
-            //         // Color.fromARGB(185, 102, 195, 235),
-            //         // Color.fromARGB(184, 13, 110, 151),
-            //       ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: 240,
+              child: Text(
+                locale.welcome,
+                textAlign: TextAlign.center,
+                style: theme.largeTitle(context),
+              ),
+            ),
+            const SizedBox(height: 18),
+            button.filled(
+              label: locale.addAHome,
+              onPressed: () => Navigator.pushReplacementNamed(
+                  context, NavigationRouteNames.discovery),
+            ),
+          ],
         ),
-        child: aspectRatio > 1
-            ? const WelcomeLandscape()
-            : const WelcomeScreenPortrait(),
       ),
     );
   }
 }
-
-// const darkColors = [
-//   Color.fromARGB(159, 46, 237, 234),
-//   Color.fromARGB(170, 62, 175, 223),
-//   Color.fromARGB(206, 120, 63, 194),
-// ];
-
-// const lightColors = [
-//   Color.fromARGB(129, 108, 231, 217),
-//   Color.fromARGB(185, 102, 195, 235),
-//   Color.fromARGB(184, 13, 110, 151),
-// ];
