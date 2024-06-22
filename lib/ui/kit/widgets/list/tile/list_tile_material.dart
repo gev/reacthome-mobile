4a2ef0a35e682b5e1e802cda8a1c92fe17998e3b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-06-22 03:15:55 +0300

    make material lists comfortable

--- lib/ui/kit/widgets/list/tile/list_tile_material.dart
+++ lib/ui/kit/widgets/list/tile/list_tile_material.dart
@@ -14,6 +14,6 @@ class ListTileMaterial {
         leading: leading,
         trailing: trailing,
         onTap: onTap,
-        visualDensity: VisualDensity.compact,
+        visualDensity: VisualDensity.comfortable,
       );
 }
