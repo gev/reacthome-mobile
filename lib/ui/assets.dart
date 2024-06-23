86d99e78bf9ac85c85c3d779d10b60fc106b9966
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-06-23 14:37:35 +0300

    Add background assets

--- lib/ui/assets.dart
+++ lib/ui/assets.dart
@@ -2,4 +2,8 @@ class Assets {
   static const logo = 'assets/logo.svg';
   static const logoLight = 'assets/logo_light.svg';
   static const logoDark = 'assets/logo_dark.svg';
+
+  static const pattern = 'assets/pattern.svg';
+  static const patternLight = 'assets/pattern_light.svg';
+  static const patternDark = 'assets/pattern_dark.svg';
 }
