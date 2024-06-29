786cd81e95d489d5ce4f06bf81856ebd7ef1af0c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-06-30 01:20:33 +0300

    naming

--- lib/util/event_bus.dart
+++ lib/util/event_bus.dart
@@ -2,18 +2,18 @@ import 'dart:async';
 
 abstract interface class EventBus<E> {
   void emit(E event);
-  StreamSubscription<E> listen(void Function(E) onData);
+  StreamSubscription<E> listen(void Function(E) handle);
 }
 
 class GenericEventBus<E> implements EventBus<E> {
   final _controller = StreamController<E>.broadcast();
 
   @override
-  void emit(E value) => _controller.add(value);
+  void emit(E event) => _controller.add(event);
 
   @override
-  StreamSubscription<E> listen(void Function(E) onData) =>
-      _controller.stream.listen(onData);
+  StreamSubscription<E> listen(void Function(E) handle) =>
+      _controller.stream.listen(handle);
 
   void close() => _controller.close();
 }
