7afd7e857c12578a2805cfd846d66d6d69fdb4e6
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-07-05 13:29:11 +0300

    cleanup

--- lib/ui/fragments/discovery/view_models/discovery_home_list_view_model.dart
+++ lib/ui/fragments/discovery/view_models/discovery_home_list_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:reacthome/core/home/home_api.dart';
 import 'package:reacthome/core/home/home_event.dart';
-import 'package:reacthome/util/replay_latest_stream.dart';
 
 class DiscoveryHomeListViewModel {
   final Stream<HomeEvent> eventSource;
@@ -15,6 +14,5 @@ class DiscoveryHomeListViewModel {
 
   Stream<Iterable<String>> get stream => eventSource
       .where((event) => event is HomeAddedEvent || event is HomeRemovedEvent)
-      .map((event) => homes)
-      .replayLatest(homes);
+      .map((event) => homes);
 }
--- lib/util/bus.dart
+++ lib/util/bus.dart
@@ -1,21 +1,10 @@
 import 'dart:async';
 
-import 'package:reacthome/util/replay_latest_stream.dart';
-
 class Bus<T> {
-  late final Sink<T> sink;
-  late final Stream<T> stream;
   final _controller = StreamController<T>.broadcast();
 
-  Bus({T? startWith}) {
-    sink = _controller.sink;
-    switch (startWith) {
-      case null:
-        stream = _controller.stream;
-      default:
-        stream = ReplayLatestStream(startWith, _controller.stream);
-    }
-  }
+  Sink<T> get sink => _controller.sink;
+  Stream<T> get stream => _controller.stream;
 
   void close() {
     _controller.close();
