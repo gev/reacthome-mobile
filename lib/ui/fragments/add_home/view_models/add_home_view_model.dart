2b24340e5517feaaf2138b1cbdddf877f085e026
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-07-09 01:57:56 +0300

    clean

--- lib/ui/fragments/add_home/view_models/add_home_view_model.dart
+++ lib/ui/fragments/add_home/view_models/add_home_view_model.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/widgets.dart';
+import 'package:reacthome/core/home/home_api.dart';
 import 'package:reacthome/core/meta.dart';
-import 'package:reacthome/features/home/application/home_service.dart';
 import 'package:reacthome/ui/app/navigation.dart';
 import 'package:reacthome/util/navigator_extension.dart';
 
 class AddHomeViewModel {
   final BuildContext context;
 
-  final HomeService home;
+  final HomeApi home;
 
   const AddHomeViewModel(this.context, {required this.home});
 
