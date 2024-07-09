aa0a675b84084ed89fbbd05546fa77dee1777f97
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-07-10 01:12:12 +0300

    Fix alert dialog Use parent mounted context instead unmounted context of the home tile

--- lib/ui/fragments/discovery/widgets/discovery_home_list.dart
+++ lib/ui/fragments/discovery/widgets/discovery_home_list.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:reacthome/ui/app/navigation.dart';
 import 'package:reacthome/ui/fragments/discovery/view_models/discovery_view_model.dart';
+import 'package:reacthome/ui/fragments/discovery/widgets/discovery_home_add_alert.dart';
+import 'package:reacthome/ui/fragments/discovery/widgets/discovery_home_add_confirm.dart';
 import 'package:reacthome/ui/fragments/discovery/widgets/discovery_home_tile.dart';
 import 'package:reacthome/ui/fragments/home/view_models/home_list_view_model.dart';
 import 'package:reacthome/ui/fragments/home/view_models/home_view_model.dart';
 import 'package:reacthome/ui/kit/kit.dart';
+import 'package:reacthome/util/navigator_extension.dart';
 
 class DiscoveryHomeList extends StatelessWidget {
   final DiscoveryViewModel discoveryViewModel;
@@ -17,6 +21,31 @@ class DiscoveryHomeList extends StatelessWidget {
     this.homeViewModel, {
     super.key,
   });
+
+  void onHomeTileTap(
+      String id, BuildContext context, AppLocalizations locale) async {
+    final confirmed = await dialog.show<bool>(
+      context,
+      builder: (_) => DiscoveryHomeAddConfirm(id, homeViewModel),
+    );
+    if (confirmed == true) {
+      if (discoveryViewModel.tryAddHome(id)) {
+        if (context.mounted) {
+          Navigator.of(context).clearNamed(
+            NavigationRouteNames.home,
+            arguments: (id: id),
+          );
+        }
+      } else {
+        if (context.mounted) {
+          dialog.show(
+            context,
+            builder: (_) => const DiscoveryHomeAddAlert(),
+          );
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,9 +63,10 @@ class DiscoveryHomeList extends StatelessWidget {
                   children: homes
                       .map((id) => DiscoveryHomeTile(
                             id,
+                            key: ValueKey(id),
                             discoveryViewModel,
                             homeViewModel,
-                            key: ValueKey(id),
+                            onTap: () => onHomeTileTap(id, context, locale),
                           ))
                       .toList(),
                 );
--- lib/ui/fragments/discovery/widgets/discovery_home_tile.dart
+++ lib/ui/fragments/discovery/widgets/discovery_home_tile.dart
@@ -1,49 +1,22 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:reacthome/ui/app/navigation.dart';
 import 'package:reacthome/ui/fragments/discovery/view_models/discovery_view_model.dart';
-import 'package:reacthome/ui/fragments/discovery/widgets/discovery_home_add_alert.dart';
-import 'package:reacthome/ui/fragments/discovery/widgets/discovery_home_add_confirm.dart';
 import 'package:reacthome/ui/fragments/home/view_models/home_view_model.dart';
 import 'package:reacthome/ui/kit/kit.dart';
-import 'package:reacthome/util/navigator_extension.dart';
 
 class DiscoveryHomeTile extends StatelessWidget {
   final String id;
   final DiscoveryViewModel discoveryViewModel;
   final HomeViewModel homeViewModel;
+  final void Function() onTap;
 
   const DiscoveryHomeTile(
     this.id,
     this.discoveryViewModel,
     this.homeViewModel, {
+    required this.onTap,
     super.key,
   });
-
-  void onHomeTileTap(
-      String id, BuildContext context, AppLocalizations locale) async {
-    final confirmed = await dialog.show<bool>(
-      context,
-      builder: (_) => DiscoveryHomeAddConfirm(id, homeViewModel),
-    );
-    if (confirmed == true) {
-      if (discoveryViewModel.tryAddHome(id)) {
-        if (context.mounted) {
-          Navigator.of(context).clearNamed(
-            NavigationRouteNames.home,
-            arguments: (id: id),
-          );
-        }
-      } else {
-        if (context.mounted) {
-          dialog.show(
-            context,
-            builder: (_) => const DiscoveryHomeAddAlert(),
-          );
-        }
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +35,7 @@ class DiscoveryHomeTile extends StatelessWidget {
           ),
           leading:
               Icon(home.hasProject ? icon.home.filled : icon.home.outlined),
-          onTap: () => onHomeTileTap(id, context, locale),
+          onTap: onTap,
         );
       },
     );
