bcc8f1016cab5956fe0ebb7717e4a7e627453861
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-07-21 16:16:32 +0300

    more DTOs

--- lib/ui/dto/connection_ui_dto.dart
+++ lib/ui/dto/connection_ui_dto.dart
@@ -1,5 +1,9 @@
-typedef ConnectionUiDto = ({
-  bool isConnected,
-  bool isLocalConnected,
-  bool isCloudConnected,
-});
+import 'package:reacthome/core/connection/connection.dart';
+import 'package:reacthome/core/connection/connection_state.dart';
+
+class ConnectionUiDto<S> {
+  final bool isConnected;
+
+  ConnectionUiDto(Connection<S>? connection)
+      : isConnected = connection?.state == ConnectionState.connected;
+}
---
+++ lib/ui/dto/home_connection_ui_dto.dart
@@ -0,0 +1,11 @@
+class HomeConnectionUiDto {
+  final bool isConnected;
+  final bool isLocalConnected;
+  final bool isCloudConnected;
+
+  HomeConnectionUiDto({
+    required this.isConnected,
+    required this.isLocalConnected,
+    required this.isCloudConnected,
+  });
+}
