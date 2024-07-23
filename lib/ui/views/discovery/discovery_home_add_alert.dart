e3d28d0237d1d1bb5df28421466843fdb2fa7b92
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-07-23 22:36:48 +0300

    add comments

--- lib/ui/views/discovery/discovery_home_add_alert.dart
+++ lib/ui/views/discovery/discovery_home_add_alert.dart
@@ -5,6 +5,14 @@ import 'package:reacthome/ui/widgets/alert.dart';
 class DiscoveryHomeAddAlert extends StatelessWidget {
   const DiscoveryHomeAddAlert({super.key});
 
+  /// Builds the widget tree for the Discovery Home Add Alert.
+  ///
+  /// The [BuildContext] is used to access the localization resources.
+  /// Displays an alert dialog with a title and action label indicating the
+  /// inability to add the home.
+  ///
+  /// Returns a [Widget] that shows an alert dialog with the title "cantAddTheHome"
+  /// and an action label "close", both obtained from the localization resources.
   @override
   Widget build(BuildContext context) {
     final locale = AppLocalizations.of(context)!;
