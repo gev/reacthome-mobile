2427a7dd0d1132a38a848e753425452d62e41b79
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-07-24 12:25:57 +0300

    cleanup

--- lib/core/discovery/discovery.dart
+++ lib/core/discovery/discovery.dart
@@ -1,5 +1,51 @@
+import 'package:reacthome/core/discovery/discovery_event.dart';
 import 'package:reacthome/core/discovery/discovery_state.dart';
 
-abstract interface class Discovery {
-  DiscoveryState get state;
+class Discovery<S> {
+  DiscoveryState state = DiscoveryState.stopped;
+  late S _source;
+
+  DiscoveryEvent? start() {
+    switch (state) {
+      case DiscoveryState.stopped:
+        state = DiscoveryState.starPending;
+        return const DiscoveryStartRequestedEvent();
+      default:
+        return null;
+    }
+  }
+
+  DiscoveryEvent completeStart(S source) {
+    switch (state) {
+      case DiscoveryState.starPending:
+        state = DiscoveryState.running;
+        _source = source;
+        return const DiscoveryStartCompletedEvent();
+      default:
+        return DiscoveryRejectedEvent<S>(source);
+    }
+  }
+
+  DiscoveryEvent? stop() {
+    switch (state) {
+      case DiscoveryState.starPending:
+        state = DiscoveryState.stopped;
+        return const DiscoveryStopCompletedEvent();
+      case DiscoveryState.running:
+        state = DiscoveryState.stopPending;
+        return DiscoveryStopRequestedEvent<S>(_source);
+      default:
+        return null;
+    }
+  }
+
+  DiscoveryEvent? completeStop() {
+    switch (state) {
+      case DiscoveryState.stopPending:
+        state = DiscoveryState.stopped;
+        return const DiscoveryStopCompletedEvent();
+      default:
+        return null;
+    }
+  }
 }
--- lib/core/discovery/discovery_service.dart
+++ lib/core/discovery/discovery_service.dart
@@ -1,36 +1,35 @@
 import 'package:reacthome/common/emitter.dart';
 import 'package:reacthome/core/discovery/discovery.dart';
 import 'package:reacthome/core/discovery/discovery_api.dart';
-import 'package:reacthome/core/discovery/discovery_entity.dart';
 import 'package:reacthome/core/discovery/discovery_event.dart';
 import 'package:reacthome/core/discovery/discovery_state.dart';
 import 'package:reacthome/util/extensions.dart';
 
 class DiscoveryService<S> implements DiscoveryApi<S> {
   final Emitter<DiscoveryEvent> eventSink;
-  final DiscoveryEntity<S> _process;
+  final Discovery<S> discovery;
 
   const DiscoveryService({
     required this.eventSink,
-    required DiscoveryEntity<S> process,
-  }) : _process = process;
+    required this.discovery,
+  });
 
   @override
-  DiscoveryState get state => _process.state;
+  DiscoveryState get state => discovery.state;
 
   @override
-  Discovery getProcess() => _process;
+  Discovery getProcess() => discovery;
 
   @override
-  void start() => _process.start()?.let(eventSink.emit);
+  void start() => discovery.start()?.let(eventSink.emit);
 
   @override
   void completeStart(S source) =>
-      _process.completeStart(source).let(eventSink.emit);
+      discovery.completeStart(source).let(eventSink.emit);
 
   @override
-  void stop() => _process.stop()?.let(eventSink.emit);
+  void stop() => discovery.stop()?.let(eventSink.emit);
 
   @override
-  void completeStop() => _process.completeStop()?.let(eventSink.emit);
+  void completeStop() => discovery.completeStop()?.let(eventSink.emit);
 }
